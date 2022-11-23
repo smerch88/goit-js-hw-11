80cd996c0c6ec0f927fda194f3a8e6c2e4bfca61
smerch88 <maxarmyk@gmail.com>
2022-11-23 15:02:44 +0200

    update

--- src/index.js
+++ src/index.js
@@ -1,76 +1,47 @@
-import axios from "axios";
 import Notiflix from "notiflix";
-import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
 
+import { ApiServices } from "./js/api-service.js";
 import createCard from "./templates/card.hbs";
 
 const searchFormRef = document.querySelector(".search-form");
 const galleryRef = document.querySelector(".gallery");
 const loadMoreRef = document.querySelector(".load-more");
 
-let searchData = "cat";
-let pageNumber = 1;
+const apiServices = new ApiServices();
 
 const onSubmitAction = (event) => {
   event.preventDefault();
-  pageNumber = 1;
   galleryRef.innerHTML = "";
-  searchData = event.currentTarget.searchQuery.value;
-  fetchPictures().then((pictures) => {
-    const data = createCard(pictures.hits);
-    galleryRef.insertAdjacentHTML("beforeend", data);
-  });
-  console.log(searchData);
-};
-
-const fetchPictures = async () => {
-  const response = await fetch(
-    `https://pixabay.com/api/?key=31495238-8710d87c3c0eb2a83465ec547&q=${searchData}&image_type=photo&page=${pageNumber}&per_page=40&safesearch=true&orientation=horizontal`
-  );
-  const pictures = await response.json();
-  console.log(pictures.total);
-  if (pictures.total === 0) {
-    throw new Error(Notiflix.Notify.failure("Not found"));
-  } else {
-    Notiflix.Notify.success(`${pictures.total} pictures found.`);
-  }
-  return pictures;
+  apiServices.pageNumber = 1;
+  apiServices.searchData = event.currentTarget.searchQuery.value;
+  apiServices
+    .getPics()
+    .then(({ data }) => {
+      if (data.total === 0) {
+        throw new Error(Notiflix.Notify.failure("Not found"));
+      } else if (apiServices.pageNumber === 1) {
+        Notiflix.Notify.success(`${data.total} pictures found.`);
+      }
+      const dataToRender = createCard(data.hits);
+      galleryRef.insertAdjacentHTML("beforeend", dataToRender);
+    })
+    .catch((error) => console.log(error));
 };
 
 const onClickAction = (event) => {
   event.preventDefault();
-  pageNumber++;
-  console.log(pageNumber);
+  apiServices.pageNumber++;
+  console.log(apiServices.pageNumber);
   event.preventDefault();
-  fetchPictures().then((pictures) => {
-    const data = createCard(pictures.hits);
-    galleryRef.insertAdjacentHTML("beforeend", data);
-  });
+  apiServices
+    .getPics()
+    .then(({ data }) => {
+      const dataToRender = createCard(data.hits);
+      galleryRef.insertAdjacentHTML("beforeend", dataToRender);
+    })
+    .catch((error) => console.log(error));
 };
 
 searchFormRef.addEventListener("submit", onSubmitAction);
 loadMoreRef.addEventListener("click", onClickAction);
-
-galleryRef.addEventListener("click", (event) => {
-  event.preventDefault();
-  const { target } = event;
-
-  if (target.nodeName !== "IMG") {
-    return;
-  }
-});
-
-// lightbox simple
-let gallery = new SimpleLightbox(".gallery a", {
-  caption: true,
-  captionsData: "alt",
-  captionDelay: 250,
-});
-gallery.on("show.simplelightbox", function () {
-  // do something…
-});
-
-gallery.on("error.simplelightbox", function (e) {
-  console.log(e); // some usefull information
-});
---
+++ src/js/api-service.js
@@ -0,0 +1,21 @@
+// Документація https://fakestoreapi.com/docs
+// Отримати всіх користувачів https://fakestoreapi.com/products
+
+import axios from "axios";
+
+export class ApiServices {
+  #BASEURL = "https://pixabay.com/api/?key=31495238-8710d87c3c0eb2a83465ec547";
+
+  constructor() {
+    this.searchData = "cat";
+    this.pageNumber = "1";
+  }
+
+  getPics() {
+    return axios.get(
+      `${this.#BASEURL}&q=${this.searchData}&image_type=photo&page=${
+        this.pageNumber
+      }&per_page=40&safesearch=true&orientation=horizontal`
+    );
+  }
+}
